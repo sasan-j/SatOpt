3246dfb8716fd70e0f61aab7dffa55fb6a3b628b
Sasan Jafarnejad <s.jafarnejad@gmail.com>
2013-12-05 11:45:57 +0100

    one more fix regarding to launcher scripts

--- parallel/launch.sh
+++ parallel/launch.sh
@@ -3,5 +3,5 @@
 module load GCC/4.7.3
 #gcc --version
 #;./satOptMOEO -W='+chCount+' -A='+algo+' -U='+str(i)+' -V='+chInst+' -T=2'
-cd $HOME/myCodes/TaSpl/parallel/
-./satOptMOEO -W=$1 -U=$2 -V=$3 -S=$4 -T=600 -R=30
+cd $HOME/myCodes/SatOpt/parallel/
+./satOptMOEO_LPL -W=$1 -U=$2 -V=$3 -S=$4 -T=600 -R=30

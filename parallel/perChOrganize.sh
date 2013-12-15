30b07d60e543eafd3fee431a88d3be8a04b5a12e
Sasan Jafarnejad <s.jafarnejad@gmail.com>
2013-12-15 20:38:39 +0100

    Just some improvements in result

--- parallel/perChOrganize.sh
+++ parallel/perChOrganize.sh
@@ -1,53 +1,63 @@
-chCount=13
+chCount=8
+
+baseDir=result
+
+mkdir "$baseDir"/finals
+mkdir "$baseDir"/executions
+mkdir "$baseDir"/finals/ch$chCount/
+mkdir "$baseDir"/executions/ch$chCount/
+
+mv "$baseDir"/final_res* "$baseDir"/finals/ch$chCount/
+mv "$baseDir"/run_res* "$baseDir"/executions/ch$chCount/
 
 ALGO=NSGA2
 algoID=0
 
-mkdir finals/ch$chCount/$ALGO
-mv finals/ch$chCount/final_res_"$chCount"_"$algoID"_* finals/ch$chCount/$ALGO/
-mkdir finals/ch$chCount/$ALGO/txt
-mv finals/ch$chCount/$ALGO/*.txt finals/ch$chCount/$ALGO/txt/
-mkdir finals/ch$chCount/$ALGO/csv
-mv finals/ch$chCount/$ALGO/*.csv finals/ch$chCount/$ALGO/csv/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO
+mv "$baseDir"/finals/ch$chCount/final_res_"$chCount"_"$algoID"_* "$baseDir"/finals/ch$chCount/$ALGO/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/txt
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.txt "$baseDir"/finals/ch$chCount/$ALGO/txt/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/csv
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.csv "$baseDir"/finals/ch$chCount/$ALGO/csv/
 
 
-mkdir executions/ch$chCount/$ALGO
-mv executions/ch$chCount/run_res_"$chCount"_"$algoID"_* executions/ch$chCount/$ALGO/
+mkdir "$baseDir"/executions/ch$chCount/$ALGO
+mv "$baseDir"/executions/ch$chCount/run_res_"$chCount"_"$algoID"_* "$baseDir"/executions/ch$chCount/$ALGO/
 
 ALGO=SPEA2
 algoID=1
-mkdir finals/ch$chCount/$ALGO
-mv finals/ch$chCount/final_res_"$chCount"_"$algoID"_* finals/ch$chCount/$ALGO/
-mkdir finals/ch$chCount/$ALGO/txt
-mv finals/ch$chCount/$ALGO/*.txt finals/ch$chCount/$ALGO/txt/
-mkdir finals/ch$chCount/$ALGO/csv
-mv finals/ch$chCount/$ALGO/*.csv finals/ch$chCount/$ALGO/csv/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO
+mv "$baseDir"/finals/ch$chCount/final_res_"$chCount"_"$algoID"_* "$baseDir"/finals/ch$chCount/$ALGO/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/txt
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.txt "$baseDir"/finals/ch$chCount/$ALGO/txt/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/csv
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.csv "$baseDir"/finals/ch$chCount/$ALGO/csv/
 
 
-mkdir executions/ch$chCount/$ALGO
-mv executions/ch$chCount/run_res_"$chCount"_"$algoID"_* executions/ch$chCount/$ALGO/
+mkdir "$baseDir"/executions/ch$chCount/$ALGO
+mv "$baseDir"/executions/ch$chCount/run_res_"$chCount"_"$algoID"_* "$baseDir"/executions/ch$chCount/$ALGO/
 
 ALGO=IBEA
 algoID=2
-mkdir finals/ch$chCount/$ALGO
-mv finals/ch$chCount/final_res_"$chCount"_"$algoID"_* finals/ch$chCount/$ALGO/
-mkdir finals/ch$chCount/$ALGO/txt
-mv finals/ch$chCount/$ALGO/*.txt finals/ch$chCount/$ALGO/txt/
-mkdir finals/ch$chCount/$ALGO/csv
-mv finals/ch$chCount/$ALGO/*.csv finals/ch$chCount/$ALGO/csv/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO
+mv "$baseDir"/finals/ch$chCount/final_res_"$chCount"_"$algoID"_* "$baseDir"/finals/ch$chCount/$ALGO/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/txt
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.txt "$baseDir"/finals/ch$chCount/$ALGO/txt/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/csv
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.csv "$baseDir"/finals/ch$chCount/$ALGO/csv/
 
 
-mkdir executions/ch$chCount/$ALGO
-mv executions/ch$chCount/run_res_"$chCount"_"$algoID"_* executions/ch$chCount/$ALGO/
+mkdir "$baseDir"/executions/ch$chCount/$ALGO
+mv "$baseDir"/executions/ch$chCount/run_res_"$chCount"_"$algoID"_* "$baseDir"/executions/ch$chCount/$ALGO/
 
 ALGO=MOGA
 algoID=3
-mkdir finals/ch$chCount/$ALGO
-mv finals/ch$chCount/final_res_"$chCount"_"$algoID"_* finals/ch$chCount/$ALGO/
-mkdir finals/ch$chCount/$ALGO/txt
-mv finals/ch$chCount/$ALGO/*.txt finals/ch$chCount/$ALGO/txt/
-mkdir finals/ch$chCount/$ALGO/csv
-mv finals/ch$chCount/$ALGO/*.csv finals/ch$chCount/$ALGO/csv/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO
+mv "$baseDir"/finals/ch$chCount/final_res_"$chCount"_"$algoID"_* "$baseDir"/finals/ch$chCount/$ALGO/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/txt
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.txt "$baseDir"/finals/ch$chCount/$ALGO/txt/
+mkdir "$baseDir"/finals/ch$chCount/$ALGO/csv
+mv "$baseDir"/finals/ch$chCount/$ALGO/*.csv "$baseDir"/finals/ch$chCount/$ALGO/csv/
 
-mkdir executions/ch$chCount/$ALGO
-mv executions/ch$chCount/run_res_"$chCount"_"$algoID"_* executions/ch$chCount/$ALGO/
+mkdir "$baseDir"/executions/ch$chCount/$ALGO
+mv "$baseDir"/executions/ch$chCount/run_res_"$chCount"_"$algoID"_* "$baseDir"/executions/ch$chCount/$ALGO/
